53cda303a5c12400d6d26317919f874a09210c62
smdbtib <smdb.tib@outlook.com>
2022-01-07 14:36:29 -0300

    add rotas cdd component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { ClassesComponent } from './cdd/classes/classes.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', pathMatch:'full', redirectTo:'cdd'},
+  {
+    path: 'cdd',
+    loadChildren: () => import('./cdd/cdd-routing.module').then(m => m.CddRoutingModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
