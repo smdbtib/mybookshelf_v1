9ddcb64e12794d0e226c4343e9a72ebc591392af
smdbtib <smdb.tib@outlook.com>
2022-01-10 13:59:21 -0300

    add Tololbar, card and buttons

--- src/app/cdd/classes/classes.component.ts
+++ src/app/cdd/classes/classes.component.ts
@@ -10,15 +10,15 @@ export class ClassesComponent implements OnInit {
 
   genderBooks: Genders[] = [
     {_idGender: "0", nameGender: "Generalities", decimalGender: "00", booksGender: 50},
-    {_idGender: "1", nameGender: "Filosofia e Psicologia", decimalGender: "00", booksGender: 12},
-    {_idGender: "2", nameGender: "Religião", decimalGender: "00", booksGender: 14},
-    {_idGender: "3", nameGender: "Ciências Sociais", decimalGender: "00", booksGender: 45},
-    {_idGender: "4", nameGender: "Linguas", decimalGender: "00", booksGender: 32},
+    {_idGender: "1", nameGender: "Philosophy and Psychology", decimalGender: "00", booksGender: 12},
+    {_idGender: "2", nameGender: "Religion", decimalGender: "00", booksGender: 14},
+    {_idGender: "3", nameGender: "Social Sciences", decimalGender: "00", booksGender: 45},
+    {_idGender: "4", nameGender: "Languages", decimalGender: "00", booksGender: 32},
     {_idGender: "5", nameGender: "Ciências Naturais e Matimática", decimalGender: "00", booksGender: 87},
-    {_idGender: "6", nameGender: "Técnilogia e Ciências Aplicadas", decimalGender: "00", booksGender: 52},
-    {_idGender: "7", nameGender: "Arter", decimalGender: "00", booksGender: 32},
-    {_idGender: "8", nameGender: "Literatura e Retórica", decimalGender: "00", booksGender: 57},
-    {_idGender: "9", nameGender: "Geografia História e Biografia", decimalGender: "00", booksGender: 14},
+    {_idGender: "6", nameGender: "Technology and Applied Sciences", decimalGender: "00", booksGender: 52},
+    {_idGender: "7", nameGender: "Arts", decimalGender: "00", booksGender: 32},
+    {_idGender: "8", nameGender: "Literature and Rhetoric", decimalGender: "00", booksGender: 57},
+    {_idGender: "9", nameGender: "Geography History and Biography", decimalGender: "00", booksGender: 14},
   ];
 
   columnView = ['_idGender', 'nameGender', 'decimalGender'];
