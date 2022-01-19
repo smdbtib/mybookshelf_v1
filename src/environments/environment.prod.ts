6438c0d888d6384f1ec4bf06462ded8e5ea80322
smdbtib <smdb.tib@outlook.com>
2022-01-19 12:02:45 -0300

    Add form login and config firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'bookshelf-24847',
+    appId: '1:300193020933:web:244a3ecbbf0f276d98306c',
+    storageBucket: 'bookshelf-24847.appspot.com',
+    apiKey: 'AIzaSyAs-y_fuVBJqQLABD5W-PRgfzbL4ZB-RvQ',
+    authDomain: 'bookshelf-24847.firebaseapp.com',
+    messagingSenderId: '300193020933',
+  },
   production: true
 };
